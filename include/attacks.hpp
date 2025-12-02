ecad482b8c151c81eb81289d4dd827b997473b0c
Jeccoman <jeccopatrickoutlook.com>
2025-12-03 02:55:29 +0300

    Enhance attack functions with additional parameters for hash usage and introduce new attack methods

--- include/attacks.hpp
+++ include/attacks.hpp
@@ -2,8 +2,13 @@
 #define ATTACKS_HPP
 
 #include <string>
+#include "hash_utils.hpp"
 
-void runBruteForce(const std::string& target, int maxLength, const std::string& charset);
-void runDictionaryAttack(const std::string& target, const std::string& dictPath);
+void runBruteForce(const std::string& target, int maxLength, const std::string& charset, bool useHash = false);
+void runDictionaryAttack(const std::string& target, const std::string& dictPath, bool useHash = false);
+void runHybridAttack(const std::string& target, const std::string& dictPath, bool useHash = false, int maxDigits = 3);
+void runRuleBasedAttack(const std::string& target, const std::string& dictPath, bool useHash = false);
+void runRainbowAttack(const std::string& targetHash, const std::string& tablePath, const std::string& charset, int maxLen);
+void runRainbowTableGeneration(const std::string& outputPath, const std::string& charset, int maxLen, int chainLength, int numChains, HashType hashType);
 
 #endif

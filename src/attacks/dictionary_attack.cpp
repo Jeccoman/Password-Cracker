098fa352a84105be9a6fc1ae4a99f1027a67213b
Jeccoman <jeccopatrickoutlook.com>
2025-12-03 02:57:58 +0300

    Refactor dictionary attack to support hash detection and comparison

--- src/attacks/dictionary_attack.cpp
+++ src/attacks/dictionary_attack.cpp
@@ -2,14 +2,29 @@
 #include <fstream>
 #include <string>
 #include <chrono>
+#include "hash_utils.hpp"
 using namespace std;
 using namespace std::chrono;
 
-void runDictionaryAttack(const string& target, const string& dictPath) {
+void runDictionaryAttack(const string& target, const string& dictPath, bool useHash) {
     ifstream dictFile(dictPath);
     if (!dictFile.is_open()) {
         cerr << "[!] Failed to open dictionary file: " << dictPath << endl;
         return;
+    }
+
+    HashType hashType = HashType::MD5;
+    if (useHash) {
+        hashType = detectHashType(target);
+        cout << "\n[+] Hash detected: ";
+        switch(hashType) {
+            case HashType::MD5: cout << "MD5"; break;
+            case HashType::SHA1: cout << "SHA1"; break;
+            case HashType::SHA256: cout << "SHA256"; break;
+            case HashType::NTLM: cout << "NTLM"; break;
+            case HashType::BCRYPT: cout << "BCRYPT"; break;
+        }
+        cout << endl;
     }
 
     cout << "\n[+] Starting Dictionary Attack...\n";
@@ -21,7 +36,16 @@ void runDictionaryAttack(const string& target, const string& dictPath) {
 
     while (getline(dictFile, word)) {
         attempts++;
-        if (word == target) {
+        bool isMatch = false;
+
+        if (useHash) {
+            string hash = computeHash(word, hashType);
+            isMatch = (hash == target);
+        } else {
+            isMatch = (word == target);
+        }
+
+        if (isMatch) {
             found = true;
             cout << "\n[!] Password cracked: " << word << endl;
             cout << "[!] Attempts: " << attempts << endl;
@@ -35,7 +59,7 @@ void runDictionaryAttack(const string& target, const string& dictPath) {
     auto duration = duration_cast<seconds>(end - start);
 
     if (!found) {
-        cout << " Password not found in dictionary.\n";
+        cout << "[!] Password not found in dictionary.\n";
     }
 
     cout << "[!] Time taken: " << duration.count() << " seconds\n";
